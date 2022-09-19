eefee344bbedd1ac6f963c5f1ec547ddae3c1103
Malik Fareed <fareed2109496@gmail.com>
2022-09-19 10:50:19 +0500

    First commit: Working on Toolbar

--- app/src/main/java/com/malik/msd/MainActivity.java
+++ app/src/main/java/com/malik/msd/MainActivity.java
@@ -1,14 +1,25 @@
 package com.malik.msd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        //giving our toolbar's reference to use in app
+        setSupportActionBar(toolbar);
+        //getting our toolbar to perform actions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //enabling BACK button
+
+        toolbar.setTitle("MSD");
     }
 }
