dd3ee43657484c0c0fbb22eafbf138e2f0913f57
Malik Fareed <fareed2109496@gmail.com>
2022-09-23 12:33:51 +0500

    Back stack functionality done

--- app/src/main/java/com/malik/msd/RateFragment.java
+++ app/src/main/java/com/malik/msd/RateFragment.java
@@ -4,62 +4,49 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RateFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    DatePicker dp;
+    RadioButton rb;
 
     public RateFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RateFragment newInstance(String param1, String param2) {
         RateFragment fragment = new RateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rate, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_rate, container, false);
+
+        dp = rootView.findViewById(R.id.datePicker);
+        rb = rootView.findViewById(R.id.rbEvening);
+
+        String date = String.valueOf(dp.getDayOfMonth())+ "/" + String.valueOf(dp.getMonth() + 1) + "/" + String.valueOf(dp.getYear());
+        Log.d("Day", date);
+
+
+        return rootView;
     }
 
 }
