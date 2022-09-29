de5d59a07698c88d5adaaaf76944347a34a43d35
Malik Fareed <fareed2109496@gmail.com>
2022-09-29 22:55:19 +0500

    Working on upload functionality

--- app/src/main/java/com/malik/msd/Entry.java
+++ app/src/main/java/com/malik/msd/Entry.java
@@ -2,14 +2,14 @@ package com.malik.msd;
 
 import java.util.Date;
 
-public class Record {
+public class Entry {
     private String date;
     private String time;
     private double rate;
     private double mann;
     private double sair;
 
-    public Record(String  _date, String _time, double _rate, double _mann, double _sair)
+    public Entry(String  _date, String _time, double _rate, double _mann, double _sair)
     {
         this.date = _date;
         this.time = _time;
--- app/src/main/java/com/malik/msd/RateFragment.java
+++ app/src/main/java/com/malik/msd/RateFragment.java
@@ -35,7 +35,7 @@ public class RateFragment extends Fragment {
     TextView tvAmount;
     Button btnAdd, btnUpload;
 
-    List<Record> records;
+    List<Entry> entryList;
 
     FragmentManager fragment;
 
@@ -51,7 +51,7 @@ public class RateFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        records = new ArrayList<Record>();
+        entryList = new ArrayList<Entry>();
 
         super.onCreate(savedInstanceState);
 
@@ -72,7 +72,7 @@ public class RateFragment extends Fragment {
         btnAdd = rootView.findViewById(R.id.btnAdd);
         btnUpload = rootView.findViewById(R.id.btnUpload);
 
-        etSair.setFilters(new InputFilter[]{new InputFilterMinMax("0","39")});
+        etSair.setFilters(new InputFilter[]{new InputFilterMinMax("0", "39")});
 
         Main();
 
@@ -80,6 +80,13 @@ public class RateFragment extends Fragment {
     }
 
     private void Main() {
+
+        entryList.add(new Entry("01/12/21", "Morning", 1000, 1, 1));
+        entryList.add(new Entry("01/12/2021", "Evening", 2000, 12, 21));
+        entryList.add(new Entry("02/12/2021", "Evening", 7500, 3, 31));
+        entryList.add(new Entry("02/12/21", "Morning", 7000, 1, 1));
+
+        toggleUploadButton();
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,6 +111,8 @@ public class RateFragment extends Fragment {
 
                     if (AddEntry(date, time, rate, mann, sair)) {
                         Toast.makeText(getActivity(), "Successfully added", Toast.LENGTH_SHORT).show();
+                        toggleUploadButton();
+                        ResetViews();
                     } else {
                         Toast.makeText(getActivity(), "Failed to add entry", Toast.LENGTH_SHORT).show();
 
@@ -112,6 +121,13 @@ public class RateFragment extends Fragment {
                 } catch (Exception e) {
                     Toast.makeText(getActivity(), "Invalid values found!", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowAlertDialog();
             }
         });
 
@@ -228,31 +244,7 @@ public class RateFragment extends Fragment {
     }
 
     private boolean AddEntry(String _date, String _time, double _rate, double _mann, double _sair) {
-        records.add(new Record(_date, _time, _rate, _mann, _sair));
-        return true;
-
-//        String date = GetFormattedDate(String.valueOf(dp.getDayOfMonth()) + "/" + String.valueOf(dp.getMonth() + 1) + "/" + String.valueOf(dp.getYear()));
-//        String time = "Morning";
-//        if (rbEvening.isChecked())
-//            time = "Evening";
-//
-//        try {
-//            if (etRate != null && etMann != null && etSair != null) {
-//                records.add(new Record(
-//                        date,
-//                        time,
-//                        Double.parseDouble(etRate.getText().toString()),
-//                        Double.parseDouble(etMann.getText().toString()),
-//                        Double.parseDouble(etSair.getText().toString())
-//                ));
-//                ShowAlertDialog();
-//            } else {
-//                Toast.makeText(this.getActivity(), "Pls Enter values.", Toast.LENGTH_SHORT).show();
-//            }
-//
-//        } catch (Exception e) {
-//            Toast.makeText(this.getActivity(), "Parse Exception: Invalid values", Toast.LENGTH_SHORT).show();
-//        }
+        return entryList.add(new Entry(_date, _time, _rate, _mann, _sair));
     }
 
     private void ResetViews() {
@@ -279,25 +271,53 @@ public class RateFragment extends Fragment {
     }
 
     private void ShowAlertDialog() {
-        if (records != null) {
+        if (entryList != null) {
             AlertDialog.Builder alert = new AlertDialog.Builder(this.getContext());
-            alert.setTitle("Data");
+            alert.setTitle("Message");
 
-            String message = "Date: " + records.get(0).getDate() + "\n Time: " + records.get(0).getTime();
+            String message = entryList.size() + " new entry(s) will be uploaded";
             alert.setMessage(message);
 
-            alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            alert.setPositiveButton("Upload", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    Toast.makeText(getActivity(), "Data Saved", Toast.LENGTH_SHORT).show();
-                    ResetViews();
+                    boolean uploaded = uploadData(entryList);
+                    if (uploaded) {
+                        entryList.clear();
+                        toggleUploadButton();
+                        Toast.makeText(getActivity(), "Upload successful.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Upload failed!.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            alert.setNegativeButton("Cancel", null);
+
+            alert.setNeutralButton("View", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+
                 }
             });
 
             alert.create().show();
         } else {
-            Toast.makeText(this.getContext(), "No data to save!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this.getContext(), "No entry found!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private boolean uploadData(List<Entry> entryList) {
+
+        return true;
+    }
+
+    private void toggleUploadButton() {
+        if (entryList.size() > 0) {
+            btnUpload.setEnabled(true);
+        } else
+            btnUpload.setEnabled(false);
     }
 
 
