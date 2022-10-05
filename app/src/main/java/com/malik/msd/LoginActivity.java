305a925e8b8e6982c057b30ae8a241cad4df3e54
Malik Fareed <fareed2109496@gmail.com>
2022-10-05 19:04:43 +0500

    Firestore: Implementing Login

--- app/src/main/java/com/malik/msd/LoginActivity.java
+++ app/src/main/java/com/malik/msd/LoginActivity.java
@@ -1,19 +1,44 @@
 package com.malik.msd;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import utils.MSDApi;
+
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin, btnCreateAcctLogin;
-    EditText etEmail, etPassword;
+    private EditText etEmail, etPassword;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
     
 
     @Override
@@ -25,13 +50,14 @@ public class LoginActivity extends AppCompatActivity {
         btnCreateAcctLogin = findViewById(R.id.btnCreateAccountLogin);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
         
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(etEmail.getText().toString()) || TextUtils.isEmpty(etPassword.getText().toString())){
-                    Toast.makeText(LoginActivity.this, "Both required!", Toast.LENGTH_SHORT).show();
-                }
+                loginUser(etEmail.getText().toString().trim(),etPassword.getText().toString().trim());
+
             }
         });
 
@@ -43,5 +69,51 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void loginUser(String _email, String _password){
+        if ( !TextUtils.isEmpty(_email) && !TextUtils.isEmpty(_password) ){
+            firebaseAuth.signInWithEmailAndPassword(_email,_password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            currentUser = firebaseAuth.getCurrentUser();
+                            String currentUserId = currentUser.getUid();
+
+                            collectionReference.whereEqualTo("userId", currentUserId)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                            if (error != null)
+                                                return;
+
+                                            assert value != null;
+                                            if (!value.isEmpty()){
+                                                for (QueryDocumentSnapshot snapshot : value) {
+                                                    MSDApi msdApi = MSDApi.getInstance();
+                                                    msdApi.setUsername(snapshot.getString("username"));
+                                                    msdApi.setUserId(snapshot.getString("userId"));
+
+                                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                                    
+                                                }
+
+
+                                            }
+                                        }
+                                    });
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+        }else {
+            Toast.makeText(LoginActivity.this, "Please enter email and password!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
