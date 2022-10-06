a1cffe2f1a98c6e9fdbe9e5c8c71ff50d91675ed
Malik Fareed <fareed2109496@gmail.com>
2022-10-06 19:10:48 +0500

    Firestore: Implementing Login

--- app/src/main/java/com/malik/msd/RecordsFragment.java
+++ app/src/main/java/com/malik/msd/RecordsFragment.java
@@ -2,22 +2,32 @@ package com.malik.msd;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.JarOutputStream;
 
 import model.Entry;
 import ui.RecordsRecyclerAdapter;
+import utils.MSDApi;
 
 public class RecordsFragment extends Fragment {
 
@@ -29,8 +39,10 @@ public class RecordsFragment extends Fragment {
     private CollectionReference collectionReference = db.collection("Records");
 
     private List<Entry> entryList;
-    private RecyclerView recyclerView;
+    private RecyclerView recordsRecyclerView;
     private RecordsRecyclerAdapter recordsRecyclerAdapter;
+
+    private TextView tvNoRecords;
 
     public RecordsFragment() {
         // Required empty public constructor
@@ -39,6 +51,12 @@ public class RecordsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        user = firebaseAuth.getCurrentUser();
+
+        entryList = new ArrayList<>();
     }
 
     @Override
@@ -46,8 +64,45 @@ public class RecordsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_recods, container, false);
 
+        tvNoRecords = rootView.findViewById(R.id.tvNoRecords);
+
+        recordsRecyclerView = rootView.findViewById(R.id.recordsRecyclerView);
+        recordsRecyclerView.setHasFixedSize(true);
+        recordsRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
         // Inflate the layout for this fragment
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        collectionReference.whereEqualTo("userId", MSDApi.getInstance()
+                .getUserId())
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()){
+                            for (QueryDocumentSnapshot records : queryDocumentSnapshots){
+                                Entry entry = records.toObject(Entry.class);
+                                entryList.add(entry);
+                            }
+
+                            recordsRecyclerAdapter = new RecordsRecyclerAdapter(getContext(), entryList);
+                            recordsRecyclerView.setAdapter(recordsRecyclerAdapter);
+                            recordsRecyclerAdapter.notifyDataSetChanged();
+                        }else {
+                            tvNoRecords.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 }
