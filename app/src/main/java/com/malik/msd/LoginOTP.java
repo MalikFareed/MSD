1d42c43e164d898b29f45d42fa7df655fe35bfd6
Malik Fareed <fareed2109496@gmail.com>
2023-02-16 23:07:03 +0500

    Firestore: Working on OTPs

--- app/src/main/java/com/malik/msd/LoginOTP.java
+++ app/src/main/java/com/malik/msd/LoginOTP.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,18 +17,32 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.concurrent.TimeUnit;
 
-public class loginOTP extends AppCompatActivity implements View.OnClickListener {
+import utils.MSDApi;
+
+public class LoginOTP extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etUsernameLoginOTP, etPhoneNumberLoginOTP, etOTPLoginOTP;
     private Button btnNextLoginOTP, btnVerifyLoginOTP;
 
-    FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     private String verificationID;
 
@@ -43,10 +58,53 @@ public class loginOTP extends AppCompatActivity implements View.OnClickListener 
         btnNextLoginOTP = findViewById(R.id.btnNextLoginOTP);
         btnVerifyLoginOTP = findViewById(R.id.btnVerifyLoginOTP);
 
-        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         btnNextLoginOTP.setOnClickListener(this);
         btnVerifyLoginOTP.setOnClickListener(this);
+
+        loadUserData();
+    }
+
+    private void loadUserData() {
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                if (currentUser != null) {
+                    currentUser = firebaseAuth.getCurrentUser();
+                    String currentUserId = currentUser.getUid();
+
+                    collectionReference
+                            .whereEqualTo("userId", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                                    if (e != null)
+                                        return;
+
+                                    if (!queryDocumentSnapshots.isEmpty()) {
+                                        for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                            MSDApi msdApi = MSDApi.getInstance();
+                                            msdApi.setUserId(snapshot.getString("userId"));
+                                            msdApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(LoginOTP.this, MainActivity.class));
+                                            finish();
+
+                                        }
+                                    }
+                                }
+                            });
+
+                } else {
+                    //do nothing to stay in same activity.
+                }
+
+            }
+        };
     }
 
     @Override
@@ -80,7 +138,7 @@ public class loginOTP extends AppCompatActivity implements View.OnClickListener 
     private void sendVerificationCode(String phoneNumber) {
 
         PhoneAuthOptions options =
-                PhoneAuthOptions.newBuilder(mAuth)
+                PhoneAuthOptions.newBuilder(firebaseAuth)
                         .setPhoneNumber("+92" + phoneNumber)       // Phone number to verify
                         .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
                         .setActivity(this)                 // Activity (for callback binding)
@@ -104,7 +162,7 @@ public class loginOTP extends AppCompatActivity implements View.OnClickListener 
 
         @Override
         public void onVerificationFailed(@NonNull FirebaseException e) {
-            Toast.makeText(loginOTP.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginOTP.this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -125,15 +183,15 @@ public class loginOTP extends AppCompatActivity implements View.OnClickListener 
     }
 
     private void signInByCredentials(PhoneAuthCredential _credential) {
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+//        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.signInWithCredential(_credential)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(loginOTP.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginOTP.this, "Login Successful", Toast.LENGTH_SHORT).show();
 
-                            Intent i = new Intent(loginOTP.this, MainActivity.class);
+                            Intent i = new Intent(LoginOTP.this, MainActivity.class);
                             i.putExtra("username", etUsernameLoginOTP.getText().toString().replace(" ", ""));
                             i.putExtra("phoneNumber", etPhoneNumberLoginOTP.getText().toString().replace(" ", ""));
                             startActivity(i);
@@ -142,5 +200,27 @@ public class loginOTP extends AppCompatActivity implements View.OnClickListener 
                         }
                     }
                 });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+
+        if (currentUser != null) {
+            startActivity(new Intent(LoginOTP.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
